59cf47151ea1640d6953722c00f75286d481b4c5
SolarLune <solarlune@gmail.com>
2022-01-06 22:20:24 -0800

    Lighting optimizations. Lighting is now done on a per-triangle basis rather than a per-vertex basis, as Sun lights color all vertices the same (as they all belong to the same triangle, which has just one normal). Point lights are also optimized - rather than transforming each vertex by the model's transform, it's faster to transform the point light's position by the model's inverted transform. Light.Light() returns an array of 9 floats, indicating the R, G, and B values for the light for each vertex of a given triangle. Adding toggle-able lighting in the lighting example.

--- light.go
+++ light.go
@@ -8,9 +8,16 @@ import (
 
 // Light represents an interface that is fulfilled by an object that emits light, returning the color a vertex should be given that Vertex and its model matrix.
 type Light interface {
-	begin()
-	Light(vertex *Vertex, model Matrix4) (float32, float32, float32)
-	isOn() bool
+	// beginRender() is used to call any set-up code or to prepare math structures that are used when lighting the scene.
+	// It gets called once when first rendering a set of Nodes.
+	beginRender()
+
+	// beginModel() is, similarly to beginRender(), used to prepare or precompute any math necessary when lighting the scene.
+	// It gets called once before lighting all visible triangles of a given Model.
+	beginModel(model *Model)
+
+	Light(tri *Triangle) [9]float32 // Light() returns the R, G, and B colors used to light the three vertices of the given triangle (and so, it returns a 9 length float32 array)
+	isOn() bool                     // isOn() is simply used to tell if a "generic" Light is on or not.
 }
 
 //---------------//
@@ -35,8 +42,6 @@ func NewAmbientLight(name string, r, g, b, energy float32) *AmbientLight {
 	}
 }
 
-func (amb *AmbientLight) begin() {}
-
 func (amb *AmbientLight) Clone() INode {
 
 	clone := NewAmbientLight(amb.name, amb.Color.R, amb.Color.G, amb.Color.B, amb.Energy)
@@ -51,9 +56,17 @@ func (amb *AmbientLight) Clone() INode {
 
 }
 
-// Light returns the global light level for the ambient light. It doesn't use the vertex or model arguments; this is just to make it adhere to the Light interface.
-func (amb *AmbientLight) Light(vertex *Vertex, model Matrix4) (float32, float32, float32) {
-	return amb.Color.R * amb.Energy, amb.Color.G * amb.Energy, amb.Color.B * amb.Energy
+func (amb *AmbientLight) beginRender() {}
+
+func (amb *AmbientLight) beginModel(model *Model) {}
+
+// Light returns the light level for the ambient light. It doesn't use the provided Triangle; it takes it as an argument to simply adhere to the Light interface.
+func (amb *AmbientLight) Light(tri *Triangle) [9]float32 {
+	return [9]float32{
+		amb.Color.R * amb.Energy, amb.Color.G * amb.Energy, amb.Color.B * amb.Energy,
+		amb.Color.R * amb.Energy, amb.Color.G * amb.Energy, amb.Color.B * amb.Energy,
+		amb.Color.R * amb.Energy, amb.Color.G * amb.Energy, amb.Color.B * amb.Energy,
+	}
 }
 
 // AddChildren parents the provided children Nodes to the passed parent Node, inheriting its transformations and being under it in the scenegraph
@@ -75,16 +88,24 @@ func (amb *AmbientLight) isOn() bool {
 
 //---------------//
 
-// PointLight represents a point light of infinite point-ness.
+// PointLight represents a point light (naturally).
 type PointLight struct {
 	*Node
-	Distance float64 // Distance represents the distance after which the light fully attenuates. If this is 0 (the default), it falls off using something akin to the inverse square law.
-	Color    *Color  // Color is the color of the PointLight.
-	// Energy is the overall energy of the Light. Internally, technically there's no difference between a brighter color and a
-	// higher energy, but this is here for convenience / adherance to GLTF / 3D modelers.
-	Energy     float32
-	On         bool // If the light is on and contributing to the scene.
-	vectorPool *VectorPool
+	// Distance represents the distance after which the light fully attenuates. If this is 0 (the default),
+	// it falls off using something akin to the inverse square law.
+	Distance float64
+	// Color is the color of the PointLight.
+	Color *Color
+	// Energy is the overall energy of the Light, with 1.0 being full brightness. Internally, technically there's no
+	// difference between a brighter color and a higher energy, but this is here for convenience / adherance to the
+	// GLTF spec and 3D modelers.
+	Energy float32
+	// If the light is on and contributing to the scene.
+	On bool
+
+	vectorPool                      *VectorPool
+	workingPosition                 vector.Vector
+	workingModelRotationalTransform Matrix4
 }
 
 // NewPointLight creates a new Point light.
@@ -94,11 +115,12 @@ func NewPointLight(name string, r, g, b, energy float32) *PointLight {
 		Distance:   0,
 		Energy:     energy,
 		Color:      NewColor(r, g, b, 1),
-		vectorPool: NewVectorPool(2),
+		vectorPool: NewVectorPool(6),
 		On:         true,
 	}
 }
 
+// Clone returns a new clone of the given point light.
 func (point *PointLight) Clone() INode {
 
 	clone := NewPointLight(point.name, point.Color.R, point.Color.G, point.Color.B, point.Energy)
@@ -114,53 +136,57 @@ func (point *PointLight) Clone() INode {
 
 }
 
-func (point *PointLight) begin() {
+func (point *PointLight) beginRender() {}
 
+func (point *PointLight) beginModel(model *Model) {
+
+	// The rotational transform is used to transform the normal of a triangle when lighting. We can't use model.Transform() for this
+	// directly because that would transpose the triangle's normal vector, effectively "moving" it.
+	point.workingModelRotationalTransform = model.Transform().SetRow(3, vector.Vector{0, 0, 0, 1})
+
+	// Rather than transforming all vertices of all triangles of a mesh, we can just transform the
+	// point light's position by the inversion of the model's transform to get the same effect and save processing time.
+	// The same technique is used for Sphere - Triangle collision in bounds.go.
+	point.workingPosition = model.Transform().Inverted().MultVec(point.WorldPosition())
 }
 
-// Light returns the R, G, and B values for the point light given the vertex and model matrix provided.
-func (point *PointLight) Light(vertex *Vertex, model Matrix4) (float32, float32, float32) {
+// Light returns the R, G, and B values for the PointLight for all vertices of a given Triangle.
+func (point *PointLight) Light(tri *Triangle) [9]float32 {
 
 	point.vectorPool.Reset()
 
-	lightPos := point.WorldPosition()
-	vertPos := point.vectorPool.MultVec(model, vertex.Position)
+	vertColors := [9]float32{}
 
-	// Model movement shouldn't be taken into account for normal adjustment
-	model = model.SetRow(3, vector.Vector{0, 0, 0, 1})
+	normal := point.vectorPool.MultVec(point.workingModelRotationalTransform, tri.Normal)
 
-	normal := point.vectorPool.MultVec(model, vertex.triangle.Normal)
+	for i, vert := range tri.Vertices {
 
-	lightVec := fastVectorSub(lightPos, vertPos).Unit()
+		lightVec := fastVectorSub(point.workingPosition, vert.Position).Unit()
 
-	diffuse := normal.Dot(lightVec)
-	if diffuse < 0 {
-		diffuse = 0
+		diffuse := normal.Dot(lightVec)
+		if diffuse < 0 {
+			diffuse = 0
+		}
+
+		var diffuseFactor float64
+		distance := fastVectorDistanceSquared(point.workingPosition, vert.Position)
+
+		if point.Distance == 0 {
+			diffuseFactor = diffuse * (1.0 / (1.0 + (0.1 * distance))) * 2
+		} else {
+			pd := math.Pow(point.Distance, 2)
+			diffuseFactor = diffuse * math.Max(math.Min(1.0-(math.Pow((distance/pd), 4)), 1), 0)
+		}
+
+		vertColors[(i * 3)] = point.Color.R * float32(diffuseFactor) * point.Energy
+		vertColors[(i*3)+1] = point.Color.G * float32(diffuseFactor) * point.Energy
+		vertColors[(i*3)+2] = point.Color.B * float32(diffuseFactor) * point.Energy
+
 	}
 
-	var diffuseFactor float64
-	distance := fastVectorDistanceSquared(lightPos, vertPos)
-
-	if point.Distance == 0 {
-		diffuseFactor = diffuse * (1.0 / (1.0 + (0.1 * distance))) * 2
-	} else {
-		pd := math.Pow(point.Distance, 2)
-		diffuseFactor = diffuse * math.Max(math.Min(1.0-(math.Pow((distance/pd), 4)), 1), 0)
-	}
-
-	// diffuseFactor := diffuse * (1.0 / (1.0 + (0.25 * distance * distance / (point.Distance * point.Distance))))
-
-	return point.Color.R * float32(diffuseFactor) * point.Energy, point.Color.G * float32(diffuseFactor) * point.Energy, point.Color.B * float32(diffuseFactor) * point.Energy
+	return vertColors
 
 }
-
-// func (point *PointLight) Light(vertex *Vertex, model Matrix4) (float32, float32, float32) {
-
-// 	var diffuseFactor float64
-
-// 	return point.Color.R * float32(diffuseFactor) * point.Energy, point.Color.G * float32(diffuseFactor) * point.Energy, point.Color.B * float32(diffuseFactor) * point.Energy
-
-// }
 
 // AddChildren parents the provided children Nodes to the passed parent Node, inheriting its transformations and being under it in the scenegraph
 // hierarchy. If the children are already parented to other Nodes, they are unparented before doing so.
@@ -190,7 +216,8 @@ type DirectionalLight struct {
 	Energy float32
 	On     bool // If the light is on and contributing to the scene.
 
-	forward vector.Vector // internal forward vector so we don't have to calculate it for every vertex for every model using this light
+	workingForward                  vector.Vector // Internal forward vector so we don't have to calculate it for every triangle for every model using this light.
+	workingModelRotationalTransform Matrix4       // Similarly, this is an internal rotational transform (without the transformation row) for the Model being lit.
 }
 
 // NewDirectionalLight creates a new Directional Light with the specified RGB color and energy (assuming 1.0 energy is standard / "100%" lighting).
@@ -203,6 +230,7 @@ func NewDirectionalLight(name string, r, g, b, energy float32) *DirectionalLight
 	}
 }
 
+// Clone returns a new DirectionalLight clone from the given DirectionalLight.
 func (sun *DirectionalLight) Clone() INode {
 
 	clone := NewDirectionalLight(sun.name, sun.Color.R, sun.Color.G, sun.Color.B, sun.Energy)
@@ -218,20 +246,26 @@ func (sun *DirectionalLight) Clone() INode {
 
 }
 
-func (sun *DirectionalLight) begin() {
-	sun.forward = sun.WorldRotation().Forward()
+func (sun *DirectionalLight) beginRender() {
+	sun.workingForward = sun.WorldRotation().Forward()
 }
 
-// Light returns the R, G, and B values for the point light given the vertex and model matrix provided.
-func (sun *DirectionalLight) Light(vertex *Vertex, model Matrix4) (float32, float32, float32) {
+func (sun *DirectionalLight) beginModel(model *Model) {
+	sun.workingModelRotationalTransform = model.Transform().SetRow(3, vector.Vector{0, 0, 0, 1})
+}
 
-	// Model movement shouldn't be taken into account for normal adjustment
+// Light returns the R, G, and B values for the DirectionalLight for each vertex of the provided Triangle.
+func (sun *DirectionalLight) Light(tri *Triangle) [9]float32 {
 
-	normal := model.SetRow(3, vector.Vector{0, 0, 0, 1}).MultVec(vertex.triangle.Normal)
+	normal := sun.workingModelRotationalTransform.MultVec(tri.Normal)
 
-	diffuseFactor := math.Max(normal.Dot(sun.forward), 0.0)
+	diffuseFactor := math.Max(normal.Dot(sun.workingForward), 0.0)
 
-	return sun.Color.R * float32(diffuseFactor) * sun.Energy, sun.Color.G * float32(diffuseFactor) * sun.Energy, sun.Color.B * float32(diffuseFactor) * sun.Energy
+	return [9]float32{
+		sun.Color.R * float32(diffuseFactor) * sun.Energy, sun.Color.G * float32(diffuseFactor) * sun.Energy, sun.Color.B * float32(diffuseFactor) * sun.Energy,
+		sun.Color.R * float32(diffuseFactor) * sun.Energy, sun.Color.G * float32(diffuseFactor) * sun.Energy, sun.Color.B * float32(diffuseFactor) * sun.Energy,
+		sun.Color.R * float32(diffuseFactor) * sun.Energy, sun.Color.G * float32(diffuseFactor) * sun.Energy, sun.Color.B * float32(diffuseFactor) * sun.Energy,
+	}
 
 }
 
