5a97763b009f4cd18f0715a8da227c2ed35c9c56
qmuntal <quimmuntal@gmail.com>
2022-03-02 14:41:12 +0100

    add gltf benchmark

---
+++ gltf_test.go
@@ -0,0 +1,23 @@
+package tetra3d
+
+import (
+	"os"
+	"testing"
+)
+
+func BenchmarkLoadGLTFData(b *testing.B) {
+	b.StopTimer()
+	data, err := os.ReadFile("./examples/logo/tetra3d.glb")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.StartTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(len(data)))
+	for i := 0; i < b.N; i++ {
+		_, err = LoadGLTFData(data, nil)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
